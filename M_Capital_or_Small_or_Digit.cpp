5b8d153f121f7640a3199d1ec44a3111d07c6961
Md: Sabbir Hossain Sourav <mdsabbirhossainsourav15@gmail.com>
2025-11-24 10:56:52 +0600

    Add files via upload

---
+++ M_Capital_or_Small_or_Digit.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    char x;
+    cin >> x;
+    if (x >= '0' && x <= '9') 
+    {
+        cout << "IS DIGIT";
+    }
+    else if (x >= 'A' && x <= 'Z')
+    {
+        cout << "ALPHA" << endl;
+        cout << "IS CAPITAL";
+    }
+    else if (x >= 'a' && x <= 'z') 
+    {
+        cout << "ALPHA" << endl;
+        cout << "IS SMALL";
+    }
+    return 0;
+}
+
